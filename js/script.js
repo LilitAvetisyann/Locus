c72d9e58241dd9f1f250b3132f1b8ce424f1a199
LilitAvetisyann <avetisyanlilit066@gmail.com>
2023-03-03 17:52:47 +0400

    add Locus whole project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+	document.getElementById("defdiv").style.display = ("block");
+    document.getElementById("logimg").style.display = ("none");
+    
+}
+function closex(){
+	document.getElementById("defdiv").style.display = ("none");
+    document.getElementById("logimg").style.display = ("block");
+}
